bacf84ea7a520835053937669aadd1c2672c7afb
Łukasz Zawadzki <lzawadzki@ZIS.local>
2023-01-27 15:29:18 +0100

    19NovemberLectureTask

---
+++ application/src/main/java/pl/edu/wszib/firstmavenapp/application/Application.java
@@ -0,0 +1,11 @@
+package pl.edu.wszib.firstmavenapp.application;
+
+import pl.edu.wszib.firstmavenapp.module1.Module1;
+import pl.edu.wszib.firstmavenapp.module2.Module2;
+
+public class Application {
+        public static void main(String[] args) {
+            Module1 a;
+            Module2 b;
+        }
+}
---
+++ module2/src/main/java/pl/edu/wszib/firstmavenapp/module2/Module2.java
@@ -0,0 +1,5 @@
+package pl.edu.wszib.firstmavenapp.module2;
+
+public class Module2 {
+
+}
